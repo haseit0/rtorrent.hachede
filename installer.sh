fd657a1099c693a390093758bc9abcac80349b1c
haseit0 <cesar.villen@gmail.com>
2015-01-30 17:03:23 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -112,7 +112,7 @@ homedir=$(cat /etc/passwd | grep "$user": | cut -d: -f6)
 #[PASO2] Actualizando e instalando dependencias
 
 apt-get update
-apt-get install openssl git subversion apache2 apache2-utils build-essential libsigc++-2.0-dev libcurl4-openssl-dev automake libtool libcppunit-dev libncurses5-dev libapache2-mod-scgi php5 php5-curl php5-cli libapache2-mod-php5 screen unrar-free unzip
+apt-get install -y openssl git subversion apache2 apache2-utils build-essential libsigc++-2.0-dev libcurl4-openssl-dev automake libtool libcppunit-dev libncurses5-dev libapache2-mod-scgi php5 php5-curl php5-cli libapache2-mod-php5 screen unrar-free unzip
 
 
 #[PASO3] Instalando XMLRPC
